7242c3979a54d98c92cbc55a64b3fa669293f3d1
Darren Tu <t-tetu@microsoft.com>
2020-07-07 17:24:03 -0400

    made changes to workflow

--- .pipelines/generateVersion.ps1
+++ .pipelines/generateVersion.ps1
@@ -1,11 +1,11 @@
+
 $major = "1"
 $minor = "0"
-# $date_patch = Get-Date -UFormat "%Y%m%d"
-$patch = $env:CDP_PATCH_NUMBER
-# $BUILD_COUNT_DAY = $env:CDP_DEFINITION_BUILD_COUNT_DAY
-$branch_name = $env:BUILD_SOURCEBRANCHNAME
+$date_patch = Get-Date -UFormat "%Y%m%d"
+$BUILD_COUNT_DAY = $env:CDP_DEFINITION_BUILD_COUNT_DAY
 $commit = $env:CDP_COMMIT_ID
-
-$buildId = "$major.$minor.$patch.$branch_name.$commit"
+$branch = $env:BUILD_SOURCEBRANCHNAME
+$build_tag = $env:CDP_BUILD_TAG
+$buildId = "$major.$minor.$date_patch.$BUILD_COUNT_DAY.$build_tag.$branch.$commit"
 [Environment]::SetEnvironmentVariable("CustomBuildNumber", $buildIdr, "User")  # This will allow you to use it from env var in later steps of the same phase
 Write-Host "##vso[build.updatebuildnumber]${buildId}"                         # This will update build ID on your build
--- .pipelines/versioning.sh
+++ .pipelines/versioning.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo "Entering into the versioning.sh command..."
-echo $BUILD_SOURCEVERSION
-echo $BUILD_SOURCEBRANCHNAME
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pwsh $DIR/generateVersion.ps1
