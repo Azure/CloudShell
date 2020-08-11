2f537ef615e9bbff8e667c1435b7fc8eb50584cf
Edwin Young <edwin@bathysphere.org>
2020-08-11 16:47:17 -0700

    checked in by accident

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-shopt -e
-
-pushd "$(dirname "$0")"
-
-docker build -t base_cloudshell -f linux/base.Dockerfile .
-docker build -t tools_cloudshell --build-arg IMAGE_LOCATION=base_cloudshell -f linux/tools.Dockerfile . 
-docker run --volume $(pwd)/tests:/tests tools_cloudshell pwsh -c "cd /tests; Install-Module -Name Pester -Force; Invoke-Pester -EnableExit"
