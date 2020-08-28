d9bf90c7a318f6355e663c133f773713d88daff2
Edwin Young <edwin@bathysphere.org>
2020-08-27 19:54:10 -0700

    just use one script

--- tests/test.sh
+++ tests/test.sh
@@ -2,13 +2,16 @@
 
 # Run the unit tests
 
+# make test files read/write by all users
+chmod -R a+rw /tests
+
 # If we don't have test user created, create them
 id -u csuser 2> /dev/null
 
 if [ $? != 0 ]
 then
   echo "Creating user"
-  /tests/setup.sh
+  adduser --disabled-login --gecos "" --uid 9527 csuser
 fi
 
 echo "running tests as csuser"
