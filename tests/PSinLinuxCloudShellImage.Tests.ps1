a2ab19952f2fa9f65a2b1c79370c8d5badab05a2
Michael Greene <mgreenegit@users.noreply.github.com>
2022-04-26 16:46:59 -0500

    Update PSinLinuxCloudShellImage.Tests.ps1

--- tests/PSinLinuxCloudShellImage.Tests.ps1
+++ tests/PSinLinuxCloudShellImage.Tests.ps1
@@ -192,12 +192,12 @@ Describe "PowerShell Modules" {
 
     }
 
-    It "Az.GuestConfiguration PowerShell Module" {
+    It "GuestConfiguration PowerShell Module" {
 
-        $module = Get-Module -Name Az.GuestConfiguration -ListAvailable
+        $module = Get-Module -Name GuestConfiguration -ListAvailable
         $module | Should -Not -BeNullOrEmpty
 
-        # Az.GuestConfiguration module version must be 0.*.* or greater
+        # GuestConfiguration module version must be 0.*.* or greater
         $module.Version -like "0.*.*" | Should -Be $true
     }
 
