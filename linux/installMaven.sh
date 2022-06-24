b1344643c3029d0954c88fed5171a8e886999ced
Michael Greene <michael.greene@microsoft.com>
2022-06-24 11:52:48 -0500

    Maven

--- linux/installMaven.sh
+++ linux/installMaven.sh
@@ -3,7 +3,7 @@
 # Customized Maven install from Apache mirrors according to best practices
 
 # Download maven from Apache Mirror
-MAVEN_VERSION=3.8.5
+MAVEN_VERSION=3.8.6
 wget https://dlcdn.apache.org/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz -P /opt
 tar xf /opt/apache-maven-$MAVEN_VERSION-bin.tar.gz -C /opt
 ln -s /opt/apache-maven-$MAVEN_VERSION /opt/maven
