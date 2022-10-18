7bfc902814a57e4eed403e69cb131132111dc833
darrentu <darrentu@gmail.com>
2022-10-18 16:45:40 -0700

    remove the helm test case

--- tests/PSinLinuxCloudShellImage.Tests.ps1
+++ tests/PSinLinuxCloudShellImage.Tests.ps1
@@ -19,8 +19,7 @@ Describe "Various programs installed with expected versions" {
     It "Static Versions" {
         # These programs are installed explicitly with specific versions
         $script:pmap["Node.JS"].Version | Should -Be '16.16.0'
-        $script:pmap["PowerShell"].Version | Should -BeLike '7.2*'
-        $script:pmap["Helm"].Version | Should -BeLike '3.9.*'       
+        $script:pmap["PowerShell"].Version | Should -BeLike '7.2*'     
     }
 
     It "Some Versions Installed" {
