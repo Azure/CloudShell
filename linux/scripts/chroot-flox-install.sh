de2101bd8ef8591150ec2c625ac73d0962a456b1
Priya Ananthasankar <priyaananthasankar@mac.lan>
2025-06-21 01:24:56 -0700

    make /nix fully writable

--- linux/scripts/chroot-flox-install.sh
+++ linux/scripts/chroot-flox-install.sh
@@ -20,16 +20,17 @@ chown -R nixuser /nix
 # Install Nix package manager
 runuser -l nixuser -c 'sh <(curl -L -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0" https://nixos.org/nix/install) --no-daemon'
 
-# Set permissions so all users can access /nix
-#chown -R root:nix-users /nix
-chmod -R 1775 /nix
-
 # Set up the paths to launch nix commands
 ln -s /home/nixuser/.nix-profile/bin/nix /usr/local/bin/nix  && ln -s /home/nixuser/.nix-profile/bin/nix-shell /usr/local/bin/nix-shell && ln -s /home/nixuser/.nix-profile/bin/nix-env /usr/local/bin/nix-env
 
 # Install flox using nix
 
 chown -R nixuser:nixuser /nix
+
+# Set permissions so all users can access /nix
+#chown -R root:nix-users /nix
+chmod -R 1777 /nix
+
 CURL_USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0" 
 runuser -l nixuser -c '. "/home/nixuser/.nix-profile/etc/profile.d/nix.sh" && nix --extra-experimental-features "nix-command flakes" profile install --accept-flake-config github:flox/flox'
 
