ee81b6e00229ffddcef824b4a4af9c46134e6954
Priya Ananthasankar <priyaananthasankar@Priyas-MacBook-Pro.local>
2025-06-23 09:20:20 -0700

    make nix world-writable

--- linux/scripts/chroot-flox-install.sh
+++ linux/scripts/chroot-flox-install.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 useradd -m -s /bin/bash nixuser
 
 # Create the /nix directory
-mkdir -m 0755 -p /nix
+mkdir -m 0777 -p /nix
 
 # Set up Nix configuration
 mkdir /home/nixuser/.config
@@ -19,6 +19,10 @@ chown -R nixuser /nix
 
 # Install Nix package manager
 runuser -l nixuser -c 'sh <(curl -L -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0" https://nixos.org/nix/install) --no-daemon'
+
+# Set permissions so all users can access /nix
+#chown -R root:nix-users /nix
+chmod -R 1777 /nix
 
 # Set up the paths to launch nix commands
 ln -s /home/nixuser/.nix-profile/bin/nix /usr/local/bin/nix  && ln -s /home/nixuser/.nix-profile/bin/nix-shell /usr/local/bin/nix-shell && ln -s /home/nixuser/.nix-profile/bin/nix-env /usr/local/bin/nix-env
