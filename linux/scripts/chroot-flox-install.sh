c6ae13c3d94f1a61fe24e3b3e8d6ef1402ff869a
Priya Ananthasankar <priyaananthasankar@mac.lan>
2025-07-11 16:44:26 -0700

    Pinned version

--- linux/scripts/chroot-flox-install.sh
+++ linux/scripts/chroot-flox-install.sh
@@ -34,9 +34,10 @@ chown -R nixuser:nixuser /nix
 # Set permissions so all users can access /nix
 #chown -R root:nix-users /nix
 chmod -R 1777 /nix
+chmod -R 1777 /nix/var/
 
 CURL_USER_AGENT="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0" 
-runuser -l nixuser -c '. "/home/nixuser/.nix-profile/etc/profile.d/nix.sh" && nix --extra-experimental-features "nix-command flakes" profile install --accept-flake-config github:flox/flox'
+runuser -l nixuser -c '. "/home/nixuser/.nix-profile/etc/profile.d/nix.sh" && nix --extra-experimental-features "nix-command flakes" profile install --accept-flake-config github:flox/flox/v1.4.4'
 
 # Symlink flox to /usr/local/bin for global access
 ln -sf /home/nixuser/.nix-profile/bin/flox /usr/local/bin/flox
