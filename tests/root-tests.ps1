8fe7bb5a1703ac7bdaee75ab119f7f22cc6215ea
Scott Kool <1058997+ComradeCow@users.noreply.github.com>
2026-07-13 10:55:05 -0500

    Force to v6, do not allow v7

--- tests/root-tests.ps1
+++ tests/root-tests.ps1
@@ -6,7 +6,7 @@ $ProgressPreference = "SilentlyContinue"
 
 Push-Location /tests
 try {
-	Install-Module -Name Pester -Force
+	Install-Module -Name Pester -MinimumVersion 6.0.0 -MaximumVersion 6.999.999 -Force
 	Invoke-Pester -CI -Path Root.Tests.ps1
 } finally {
 	Pop-Location
